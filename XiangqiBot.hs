977f3025bff1f8e1950e6d5ba77b141c84594121
Rama <ramawidyadhanabhagaskoro@gmail.com>
2022-02-20 19:24:18 +0100

    added getAdvisorMoves

--- XiangqiBot.hs
+++ XiangqiBot.hs
@@ -20,9 +20,6 @@ listMoves = _listMovesImpl_ -- YOUR IMPLEMENTATION HERE
 
 
 -- YOUR IMPLEMENTATION FOLLOWS HERE
-isSamePos :: Int -> Int -> Bool
-isSamePos piecePos tryPos = piecePos == tryPos
-
 getPosition :: Int -> Int -> Int
 getPosition  spalte zeile = spalte + (zeile * 9)
 
@@ -50,3 +47,45 @@ spalteToInt x = case x of
 
 getSpalte :: Int -> Int
 getSpalte pos = div pos 10
+
+
+getPotentialMove :: String -> Char -> Int -> Int -> Bool -> String
+getPotentialMove b p startPos zielPos checkIfChecks = do
+  case  toLower (b !! (startPos -1)) of -- trace ("player: " ++ show p ++ show (b !! (startPos -1)) ++ show startPos ++ "->" ++ show zielPos ++ " b: " ++ b)
+    'p' -> getPawnMoves b p startPos zielPos checkIfChecks
+    'k' -> getKingMoves b p startPos zielPos checkIfChecks
+    'r' -> getRookMoves b p startPos zielPos checkIfChecks
+    'n' -> getKnightMoves b p startPos zielPos checkIfChecks
+    'b' -> getBishopMoves b p startPos zielPos checkIfChecks
+    'q' -> getQueenMoves b p startPos zielPos checkIfChecks
+    '0' -> ""
+
+--redPlayer = true
+getAdvisorMoves :: String -> Bool -> [Int] -> [Int] -> String
+getAdvisorMoves board player startPos zielPos =
+    if startPos == zielPos
+        then ""
+        else 
+            if canMove && doesNotResultInCheck
+            then "," ++ [chr (startPos !! 1 + 97)] ++ [intToDigit (9 - startPos!!0)] ++ "-" ++ [chr (zielPos !! 1 + 97)] ++ [intToDigit (9-zielPos!!0)]
+            else ""
+        where
+            xStart = startPos !! 1
+            yStart = startPos !! 0
+            xZiel = zielPos !! 1
+            yZiel = zielPos !! 0
+            canMove = isInPalast player && (abs (xZiel - xStart)) == 1 && (abs (yZiel - yStart)) == 1
+    
+getSoldierMoves :: String -> Bool -> [Int] -> [Int] -> String
+getSoldierMoves board player startPos zielPos =
+    if startPos == zielPos
+        then ""
+        else 
+            if canMove && doesNotResultInCheck
+            then "," ++ [chr (startPos !! 1 + 97)] ++ [intToDigit (9 - startPos!!0)] ++ "-" ++ [chr (zielPos !! 1 + 97)] ++ [intToDigit (9-zielPos!!0)]
+            else ""
+        where
+            xStart = startPos !! 1
+            yStart = startPos !! 0
+            xZiel = zielPos !! 1
+            yZiel = zielPos !! 0
